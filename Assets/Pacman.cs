c13943eedfff70fa080a2a9c51f284026439ceb7
shakenbake444 <53156735+shakenbake444@users.noreply.github.com>
2022-08-21 23:01:32 -0400

    Pacman Spinning

--- Assets/Pacman.cs
+++ Assets/Pacman.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AnimatedSprite deathSequence;
+    public SpriteRenderer spriteRenderer;
+    // try to remove new
+    public new Collider2D collider;
+    public Movement movement;
+    void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
+        movement = GetComponent<Movement>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            movement.SetDirection(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) {
+            movement.SetDirection(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+            movement.SetDirection(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            movement.SetDirection(Vector2.right);
+        }
+
+        float angle = Mathf.Atan2(movement.direction.y, movement.direction.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, transform.forward);
         
     }
 }
+// up, y = 1, x = 0; atan = pi/2
+// down y = -1, x = 0; atan = -pi/2
+// right y = 0, x = 1; atan = 
